2f6de0eccdb9cbe8558a0e67f7a27d7580b1743f
ccelia99 <serja.aaltonen@gmail.com>
2021-03-18 14:42:58 +0200

    Teht 1.5 valmiina

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -1,60 +1,9 @@
-import React from 'react';
+import React  from 'react';
 import ReactDOM from 'react-dom';
-
-const App = () => {
-    const course = 'Half Stack application development'
-    const parts = [
-        {
-        name: 'Fundamentals of React',
-        exercises: 10
-        },
-        {
-        name: 'Using props to pass data',
-        exercises: 7
-        },
-        {
-        name: 'State of a component',
-        exercises: 14
-        }
-    ]
-
-    const Header = () => {
-        return (
-            <h1>{course}</h1>
-        )
-    }
-    
-
-    const Content = () => {
-        return(
-            <>
-            <Part part={parts[0]}  />
-            <Part part={parts[1]}  />
-            <Part part={parts[2]}  />
-            </>
-        )
-    }
-
-    const Part = (props) => {
-        return (
-            <p>{props.part.name} {props.part.exercises}</p>
-        )
-
-    }
-
-    const Total = (props) => {
-        return(
-            <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-        )
-    }
+import App from './App';
   
-    return (
-      <div>
-        <Header course={course} />
-        <Content parts={parts} />
-        <Total parts={parts} />
-      </div>
-    )
-  }
-  
-  ReactDOM.render(<App />, document.getElementById('root'))
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
