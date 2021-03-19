b4b58a5e90d3273827d430981e3a676ab60b3086
ccelia99 <serja.aaltonen@gmail.com>
2021-03-19 22:27:14 +0200

    Teht. 1.7 tehty

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,3 +1,6 @@
+//React ei paivita kenttaa all heti, joten average ja positive laskut menevat ekasta
+//klikkauksesta pieleen
+
 import React, {useState} from 'react'
 
 const DisplayHeader = props => <h1>{props.text}</h1>
@@ -21,35 +24,58 @@ const App = props => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const goodSetToValue = () => { 
-    console.log('good nappia painettu')
-    console.log(good+1)
-    setGood(good+1)
+  const [all, setAll] = useState(0)
+  const [average, setAverage]  = useState(0)
+  const [positive, setPositive] = useState(0)
+
+  const setValues = (value) => { 
+    
+    if (value >= 0) {
+      setGood(good+1)
+      console.log('good')
+      console.log(good)
+    }
+    else if (value <= 0){
+      setBad(bad+1)
+      console.log('bad')
+      console.log(bad)
+    }
+    else {
+      setBad(neutral+1)
+      console.log('neutral')
+      console.log(neutral)
+    }
+
+    setAll(all+1)
+    
+    console.log('all')
+    console.log(all)
+    
+    console.log('average')
+    console.log(average)
+
+    setAverage((good-bad)/all)
+    console.log('new average')
+    console.log(average)   
+    
+    setPositive(good*100/all)
   }
 
-  const neutralSetToValue = () => { 
-    console.log('neurtal nappia painettu')
-    console.log(neutral+1)
-    setNeutral(neutral+1)
-  }
-
-  const badSetToValue = () => { 
-    console.log('bad nappia painettu')
-    console.log(bad+1)
-    setBad(bad+1)
-  }
-
+   
 
   return (
     <div>
       <DisplayHeader text='give feedback' />
-      <Button onClick={() => goodSetToValue()} text='Good'/>
-      <Button onClick={() => neutralSetToValue()} text='Neutral' />
-      <Button onClick={() => badSetToValue() } text='Bad' />
+      <Button onClick={() => setValues(1)} text='Good'/>
+      <Button onClick={() => setValues(0)} text='Neutral' />
+      <Button onClick={() => setValues(-1) } text='Bad' />
       <DisplayHeader  text='statistics' />
       <Display text='good' value={good} />
       <Display text='neutral' value={neutral} />
       <Display text='bad' value={bad} />
+      <Display text='all' value={all} />
+      <Display text='average' value={average} />
+      <Display text='positive' value={positive} />
 
     </div>
   );
