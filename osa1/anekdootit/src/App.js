70e1535f7ac7e3b084bf7a88d30945320fedebdb
ccelia99 <serja.aaltonen@gmail.com>
2021-03-21 15:11:31 +0200

    Teht 1.13 valmiina

--- osa1/anekdootit/src/App.js
+++ osa1/anekdootit/src/App.js
@@ -5,6 +5,12 @@ const Button = (props) =>
     {props.text}
   </button>
 
+const Display = props => 
+  <>
+    <div>{props.text}</div>
+    <div>has {props.vote} votes</div>
+  </>
+const DisplayHeader = props => <h1>{props.text}</h1>
 
 
 const App = props => {
@@ -17,19 +23,38 @@ const App = props => {
     'Premature optimization is the root of all evil.',
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
   ]
- 
+  
   const [selected, setSelected] = useState(0)
+  const [voteArray, setVoteArray] = useState(Array(6).fill(0))
+  
+  
+  const setAnecdote = () => {
+    const randNum = Math.floor(Math.random() * 6)
+    setSelected(randNum)    
+    
+    console.log('anekdootin numero ', randNum)  
+  }
 
-  const setAnecdote = () => {setSelected(Math.floor(Math.random() * 6))}
+  const setVote = (value) => {    
+    let copyArray = {...voteArray}    
+    let copyItem = voteArray[selected]    
+    copyItem +=1    
+    copyArray[selected] = copyItem
+    setVoteArray(copyArray)
+
+    console.log('voteArray', voteArray)   
+  }
 
 
 
   return (
     <div>
-       <Button onClick={() => setAnecdote()} text='next anecdote'/>
-       {anecdotes[selected]}
-     
+      <DisplayHeader text='Anecdote of the day' />      
+      <Display text={anecdotes[selected]} vote={voteArray[selected]} /> 
+      <Button onClick={() => setVote(selected)} text='vote'/>
+      <Button onClick={() => setAnecdote()} text='next anecdote'/>        
     </div>
   );
 }
 export default App;
+
