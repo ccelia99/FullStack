8539ed6151e4d91842af5468443db13f20657085
ccelia99 <serja.aaltonen@gmail.com>
2021-03-23 16:16:50 +0200

    Teht. 2.2 valmiina

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 
-const Part = ({part}) => {
-  return (
-    <li>{part.name} {part.exercises} </li>
+
+const Course = ({course}) => {
+  console.log('props course', course.name) 
+  return (    
+    <>
+      <Header course={course} />
+      <Content parts={course.parts} />       
+      <Total parts={course.parts} /> 
+    </>   
   )
 }
 
+const Header = ({course}) => {
+  return (
+      <h1>{course.name}</h1>
+  )
+}  
+
 const Content = ({parts} ) => {        
-  return(
+  return (
     <>
       {parts.map( part =>
         <Part key={part.id} part={part} />
@@ -16,27 +28,30 @@ const Content = ({parts} ) => {
   )
 }
 
-const Header = ({course}) => {
+const Part = ({part}) => {
   return (
-      <h1>{course.name}</h1>
-  )
-}   
-
-const Total = ({parts}) => {
-  return(
-      <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+    <li> {part.name} {part.exercises} </li>
   )
 }
 
+const Total = ({parts}) => {
 
-const App = () => {
-    
-     
+  const result = parts.reduce( (part, item)  => {
+    return part + item.exercises
+  } , 0)
+
+  return (
+    <>
+    <p><b>Total of exercises {result} </b></p> 
+    </>
+ 
+  )}
+
+const App = ({course} ) => {
+  
     return (
       <div>
-        <Header course={course} />
-        <Content parts={course.parts} />       
-        <Total parts={course.parts} />
+        <Course course={course} />        
       </div>
     )
   }
